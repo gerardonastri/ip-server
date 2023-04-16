f3af9c99ae3a62c801780bc70ebd2dc65c087681
gerardonastri <gerardonastri06@gmail.com>
2023-04-16 10:58:13 +0200

    better

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,6 +1,17 @@
 import requestIp from "request-ip";
+import os from "os";
 
 export default function handler(req, res) {
   const clientIp = requestIp.getClientIp(req);
-  res.status(200).json(clientIp);
+  const userInfo = os.userInfo();
+  const platform = os.platform();
+  const architecture = os.arch();
+  res
+    .status(200)
+    .json({
+      ip: clientIp,
+      userInfo: userInfo,
+      platform: platform,
+      architecture: architecture,
+    });
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,19 @@ import axios from 'axios'
 
 export default function Home() {
   const [ip, setIp] = useState(null)
+  const [user, setUser] = useState(null)
+  const [platform, setPlatform] = useState(null)
+  const [architecture, setArchitecture] = useState(null)
 
   useEffect(() => {
     const getIp = async () => {
       try {
         const res = await axios.get("https://ip-server.vercel.app/api/ip")
-        setIp(res.data)
+        // const res = await axios.get("http://localhost:3000/api/ip")
+        setIp(res.data.ip)
+        setUser(res.data.userInfo)
+        setPlatform(res.data.platform)
+        setArchitecture(res.data.architecture)
       } catch (error) {
         console.log(error);
       }
@@ -18,10 +25,16 @@ export default function Home() {
     getIp()
   }, [])
 
+  console.log(user);
+
   return (
     <div>
       <h1>CIAOOOO! Sto per rubarti l'indirizzo ip</h1>
       <h1>Il tuo indirizzo è {ip && ip}</h1>
+      <h1>Il tuo nome è: {user && user.username}</h1>
+      <h1>La tua homedire è: {user && user.homedir}</h1>
+      <h1>La tua piattaforma è: {platform && platform}</h1>
+      <h1>La tua architettura è: {architecture && architecture}</h1>
     </div>
 
   )
